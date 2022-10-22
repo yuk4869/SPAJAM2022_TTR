95fd24026923696a1eef5c18ded136c3339dcf5f
sh1n00 <rinjyou0031@toki.waseda.jp>
2022-10-23 03:24:10 +0900

    add: api mockの追加

--- server/api.py
+++ server/api.py
@@ -14,14 +14,16 @@ pipeline = DetectMiniXceptionFER()
 
 @app.route("/", methods=["POST"])
 def index():
-    img_pil = Image.open(io.BytesIO(request.files["request_data"].read()))
-    img_numpy = np.asarray(img_pil)
-    img_bgr = cv2.cvtColor(img_numpy, cv2.COLOR_RGBA2BGR)
-    detect_result = pipeline(img_bgr)
-    print(detect_result)
-    if "boxes2D" in detect_result:
-        return detect_result["boxes2D"]
-    return detect_result
+    # img_pil = Image.open(io.BytesIO(request.files["request_data"].read()))
+    # img_numpy = np.asarray(img_pil)
+    # img_bgr = cv2.cvtColor(img_numpy, cv2.COLOR_RGBA2BGR)
+    # detect_result = pipeline(img_bgr)
+    frame = cv2.imread("./test1.jpg")
+    detect_result = pipeline(frame)
+    print(detect_result["boxes2D"])
+    if "boxes2D" not in detect_result:
+        return None
+    return detect_result["boxes2D"]
 
 if __name__ == "__main__":
     warnings.simplefilter('ignore')
