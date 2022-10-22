f84cf56944cbd7c9e19cbbe697cd647e4201c71c
sh1n00 <rinjyou0031@toki.waseda.jp>
2022-10-22 17:22:36 +0900

    fix: requestの受け取り方の修正

--- server/api.py
+++ server/api.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
-def index(request):
-    print(request)
+def index():
+    print(request.get_data())
     return b"Helllo World"
 
 if __name__ == "__main__":
