cb3d7f9c15324bd99464f9d2ae00326e9e4e2405
sh1n00 <rinjyou0031@toki.waseda.jp>
2022-10-23 00:14:20 +0900

    add: APIサーバーの実装

--- server/api.py
+++ server/api.py
@@ -1,12 +1,27 @@
 from flask import Flask, request
+import base64
+import io
+import warnings
+import cv2
+from PIL import Image
+import numpy as np
+
+from paz.pipelines import DetectMiniXceptionFER
 
 app = Flask(__name__)
 
+pipeline = DetectMiniXceptionFER()
+
 @app.route("/", methods=["POST"])
 def index():
-    print(request.get_data())
-    return b"Helllo World"
+    img_pil = Image.open(io.BytesIO(request.files["request_data"].read()))
+    img_numpy = np.asarray(img_pil)
+    img_bgr = cv2.cvtColor(img_numpy, cv2.COLOR_RGBA2BGR)
+    detect_result = pipeline(img_bgr)
+    if "boxes2D" in detect_result:
+        return detect_result["boxes2D"]
+    return detect_result
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    warnings.simplefilter('ignore')
+    app.run(debug=True)
